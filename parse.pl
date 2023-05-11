22d846b85150a4e6df34680b7de13ea6ecbe5532
Mark Hary <44713017+markhary@users.noreply.github.com>
2023-05-11 10:52:44 -0700

    Basic chart done

--- parse.pl
+++ parse.pl
@@ -5,8 +5,27 @@ die "Usage: parse.pl input_file output_file\n" if ($#ARGV < 1);
 $input_file = $ARGV[0];
 $output_file = $ARGV[1];
 
-print "$input_file\n";
-print "$output_file\n";
-
 open(INPUT, "<", $input_file) or die "Could not open " . $input_file . "\n";
 open(OUTPUT, ">", $output_file) or die "Could not open " . $output_file . "\n";
+
+print "parsing file..\n";
+
+$exercise_log = "EXERCISE LOG";
+
+$searching = 1;
+while (<INPUT>) {
+  chomp;
+  next if /^\s*$/; #skip blank lines
+
+  next if ($searching && /^(?:(?!$exercise_log).)*\z/s);
+  $searching = 0;
+  last if /^#####/;
+  print OUTPUT "$_\n";
+}
+
+print "done.\n";
+
+close (INPUT);
+close (OUTPUT);
+
+exit 0;
